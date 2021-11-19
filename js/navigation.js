0ecbbe2dd1884b566c372467f0c4cc81908560e4
dpay <deviap485@gmail.com>
2021-11-19 19:56:43 +0700

    alert pop up on next button

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,15 @@
 function goBack() {
     window.history.back();
 }
+
+
+function ConfirmPopUp() {
+    var txt;
+    var r = confirm("Anda Tidak dapat mengakses halaman ini lagi! Yakin Akan Melanjutkan?! ");
+    if (r == true) {
+        return true;
+    } else {
+        return false;
+
+    }
+}
