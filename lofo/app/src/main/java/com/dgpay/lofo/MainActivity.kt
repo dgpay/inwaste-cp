6307d880881d5cb0929dffbfec63d02af4cc9c4a
deviap <deviayuperamesti@gmail.com>
2022-03-28 10:09:58 +0700

    initial commit

--- lofo/app/src/main/java/com/dgpay/lofo/MainActivity.kt
+++ lofo/app/src/main/java/com/dgpay/lofo/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.dgpay.lofo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvFoods: RecyclerView
+    private var list: ArrayList<Food> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        rvFoods = findViewById(R.id.rv_foods)
+        rvFoods.setHasFixedSize(true)
+        list.addAll(FoodData.listData)
+        showRecyclerGrid()
     }
+
+    private fun showRecyclerGrid() {
+        rvFoods.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridFragmentAdapter(list)
+        rvFoods.adapter = gridHeroAdapter
+    }
+
+    private fun showRecyclerList() {
+        rvFoods.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListFoodAdapter(list)
+        rvFoods.adapter = listHeroAdapter
+    }
+
+
 }
